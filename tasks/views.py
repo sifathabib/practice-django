bc30d32c87b6994037b8e8e29f0e3e4c3c4ce257
HabiburRahman <sifathabib52@gmail.com>
2025-01-20 12:39:04 +0600

    Dynamic urls

--- tasks/views.py
+++ tasks/views.py
@@ -6,3 +6,7 @@ def home(request):
     return HttpResponse("well come to the task managment system")
 def show_task(request):
     return HttpResponse("This is our task page")
+def show_specific_task(request,id):
+    print("id",id)
+    print("id type",type(id))
+    return HttpResponse(f"this is specific task {id}")
