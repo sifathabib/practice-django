587284a2b34e1598753b6b723ce35628184f1e91
Habibur Rahman <sifathabib52@gmail.com>
2025-02-15 01:39:31 +0600

    module-8

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 def manager_dashboard(request):
     return render(request,'dashboard/manager-dashboard.html')
 def user_dashboard(request):
     return render(request,'dashboard/user-dashboard.html')
 def test(request):
     return render(request,"test.html")
+
+
